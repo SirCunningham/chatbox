5883f87c7cda41b8d7ad289c77b6000a6976f99a
Ron Paul <RonPaul@RonPaul.RonPaul>
2014-02-28 19:57:46 +0100

    asds

--- Controller.java
+++ Controller.java
@@ -389,19 +389,5 @@ public class Controller {
         }
         return new String(chars);
     }
-    public String decryptCaesar(String text, int shift) {
-        char[] chars = text.toCharArray();
-        for (int i = 0; i < text.length(); i++) {
-            char c = chars[i];
-            if (c >= 32 && c <= 127) {
-                int x = c - 32;
-                x = (x - shift) % 96;
-                if (x < 0) {
-                    x += 96;
-                }
-                chars[i] = (char) (x + 32);
-            }
-        }
-        return new String(chars);
-    }
+
 }
--- XMLString.java
+++ XMLString.java
@@ -19,4 +19,19 @@ public class XMLString {
         String hexColor = xmlStr.substring(index + 7, index + 13);
         return Color.decode("#" + hexColor);
     }
+    public String decryptCaesar(String text, int shift) {
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < text.length(); i++) {
+            char c = chars[i];
+            if (c >= 32 && c <= 127) {
+                int x = c - 32;
+                x = (x - shift) % 96;
+                if (x < 0) {
+                    x += 96;
+                }
+                chars[i] = (char) (x + 32);
+            }
+        }
+        return new String(chars);
+    }
 }
