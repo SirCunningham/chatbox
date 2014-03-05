bf07e680265118b098b040634987fdc21acebca7
Anon <anon@example.com>
2014-03-05 01:30:42 +0100

    new remove

--- ChatBox.java
+++ ChatBox.java
@@ -2,7 +2,6 @@ package chatbox;
 
 //Scrollen fungerar ej - ibland!
 //Begränsa tabbarnas tjocklek!
-//Tabbar funkar ej - samma bug som tidigare!
 public class ChatBox {
 
     public static void main(String[] args) {
