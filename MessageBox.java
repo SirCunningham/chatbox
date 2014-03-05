479b90d11b93d25cf9107075a29adc24d03b241d
Anon <anon@example.com>
2014-03-05 23:55:28 +0100

    request file

--- MessageBox.java
+++ MessageBox.java
@@ -216,7 +216,7 @@ class MessageBox {
             //Tag inte med control characters
             if (c >= 32 && c <= 127) {
                 int x = c - 32;
-                x += shift;
+                x = (x + shift) % 96;
                 if (x < 0) {
                     x += 96;
                 }
