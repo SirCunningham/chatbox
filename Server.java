d6ee6baafab30486bd1af653f5afcfb57e4f5239
SirCunningham <lvckv_v@yahoo.se>
2014-04-11 22:36:53 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,34 +6,36 @@ import java.util.*;
 
 public class Server implements Runnable {
 
-    private final ServerSocket serverSocket;
-    private final int port;
-    private final ChatRoom chatRoom;
-    private ArrayList<ChatRoom> connectedChatRooms;
-    private final Object lock = new Object();
-    private final Object lock2 = new Object();
+    private ServerSocket serverSocket;
     private Socket clientSocket;
     private LinkedList<IOThread> threads;
+    private final int port;
+    private final ChatRoom chatRoom;
+    private final Object lock = new Object();
 
-    public Server(ServerSocket serverSocket, int port,
-            final ChatRoom chatRoom) {
-        this.serverSocket = serverSocket;
+    public Server(int port, final ChatRoom chatRoom) {
         this.port = port;
         this.chatRoom = chatRoom;
-        connectedChatRooms = new ArrayList<>();
-        connectedChatRooms.add(chatRoom);
+        
+        // Starta socket för servern
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(100);
+        } catch (IOException ex) {
+            chatRoom.success = false;
+            chatRoom.showError(String.format("Could not listen on port %d.",
+                    port));
+        }
     }
 
     @Override
     public void run() {
         if (serverSocket != null) {
             threads = new LinkedList<>();
-            
             // Lyssna efter klienter
             while (chatRoom.alive) {
                 try {
                     clientSocket = serverSocket.accept();
-                    
                     // Skapa tråd för varje klient
                     synchronized (lock) {
                         threads.addLast(new IOThread(clientSocket, threads, lock));
@@ -51,22 +53,5 @@ public class Server implements Runnable {
                 chatRoom.showError("Failed to close server.");
             }
         }
-    }
-
-    public void addChatRoom(ChatRoom chatRoom) {
-        connectedChatRooms.add(chatRoom);
-    }
-    public ArrayList<ChatRoom> getChatRooms() {
-        return connectedChatRooms;
-    }
-    public void addUser(ChatRoom chatRoom) {
-        synchronized (lock2) {
-            for (ChatRoom msgBox : connectedChatRooms) {
-                if (!chatRoom.items.contains(msgBox)) {
-                    chatRoom.items.addElement(msgBox);
-                }
-            }
-        }
-
     }
 }
