4a642702faa339e989cbdb5754a8361a5a5eead5
Anon <anon@example.com>
2014-05-29 18:22:10 +0200

    unicity

--- ChatBox.java
+++ ChatBox.java
@@ -16,6 +16,9 @@ package chatbox;
 //Mellanrum skapas efter krypterad text
 //Ingen tom rad efter nekad keyrequest!
 
+//Kolla om man verkligen byter färg
+//Unikt namn!
+
 //Småfixar återstår för LeftPanel/RightPanel/ChatRoom...
 
 import java.awt.event.*;
