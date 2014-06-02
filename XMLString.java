8173120c75252344c4067037c65580d8627d166f
Ron Paul <ronpaul@ronpaul.ronpaul>
2014-06-02 15:55:55 +0200

    boot

--- XMLString.java
+++ XMLString.java
@@ -255,7 +255,7 @@ public class XMLString {
     
     public static String getFileName(String xmlMsg) {
         if (xmlMsg != null) {
-            if (xmlMsg.matches("<filerequest(.*)name=(.*)>(.*)</filerequest>")) {
+            if (xmlMsg.matches("(.*)<filerequest(.*)name=(.*)>(.*)</filerequest>(.*)")) {
                 int index = xmlMsg.indexOf("name");
                 return xmlMsg.substring(index + 6, xmlMsg.indexOf(">") - 1) + ": ";
             }
@@ -289,7 +289,7 @@ public class XMLString {
 
     public static String getSenderWithoutColon(String xmlMsg) {
         String withColon = getSender(xmlMsg);
-        if (withColon.equals("")) {
+        if (withColon == null) {
             return withColon;
         }
         return withColon.substring(0, withColon.length() - 2);
